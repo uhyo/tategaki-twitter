8a0b246bed538d0cdff1eba64ad6c23c87a18b72
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-01-25 18:42:37 +0900

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="primary">
+      <b-navbar-toggle target="nav_collapse" />
+
+      <b-navbar-brand href="#">
+        tategakiTwitter
+      </b-navbar-brand>
+
+      <b-collapse id="nav_collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="https://retoruto-carry.github.io/">
+            れとるときゃりー
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        縦書きったー
+      </h1>
+      <h2 class="subtitle">
+        ツイッターで俳句でも詠もう
+      </h2>
+      <b-form-textarea
+        v-model="inputText"
+        class="mt-4"
+        placeholder="俳句を詠もう"
+        :rows="3"
+      />
+      <b-form-textarea
+        :value="outputText"
+        class="mt-4"
+        placeholder="縦書きになって出てくる"
+        :rows="9"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: ''
+    }
+  },
+  computed: {
+    inputGyous() {
+      const inputGyous = []
+      const strings = this.inputText.split(/\r\n|\r|\n/)
+      strings.forEach(string => {
+        inputGyous.push(string)
+      })
+      return inputGyous
+    },
+    maxHeightOfinputGyous() {
+      return this.inputGyous.length
+    },
+    maxWidthOfinputGyous() {
+      return this.computeMaxWidhtOfArray(this.inputGyous)
+    },
+    outputGyous() {
+      const outputGyous = []
+
+      // 並び替える
+      for (let w = 0; w < this.maxWidthOfinputGyous; w++) {
+        let outputGyou = ''
+        for (let h = this.maxHeightOfinputGyous - 1; h >= 0; h--) {
+          let char
+          if (w + 1 > this.inputGyous[h].length) {
+            char = '　'
+          } else {
+            char = this.inputGyous[h].substr(w, 1)
+          }
+          outputGyou += char
+        }
+        outputGyous.push(outputGyou)
+      }
+
+      return outputGyous
+    },
+    outputText() {
+      let outputText = ''
+      this.outputGyous.forEach(gyou => {
+        outputText += gyou + '\r'
+      })
+      return outputText
+    }
+  },
+  methods: {
+    computeMaxWidhtOfArray(array) {
+      let maxWidthOfOutput = 0
+      array.forEach(element => {
+        if (element.length > maxWidthOfOutput) {
+          maxWidthOfOutput = element.length
+        }
+      })
+      return maxWidthOfOutput
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin-top: 2em;
+}
+</style>
